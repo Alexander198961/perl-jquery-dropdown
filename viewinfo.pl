2e1e1a42b78e2452861718164e350346a9e84a48
alexander <alexander198961@gmail.com>
2016-11-30 01:11:10 -0500

    fix

--- viewinfo.pl
+++ viewinfo.pl
@@ -6,11 +6,26 @@ use DBI;
 my $dbh = DBI->connect("DBI:mysql:database=test;host=localhost",
                          "root", "root",
                          {'RaiseError' => 1});
+#if ($action eq "view") {
 print qq(Content-type: text/html\n\n);
-my $myquery1 = "SELECT title from tutorials";
+my $query = new CGI;
+my $name=$query->param('name');
+my $myquery1 = "SELECT title from tutorials where title like '%$name%'";
 my $sth = $dbh->prepare($myquery1);
-
 $sth->execute();
+#print "<select size=\"5\">";
+my $count=0;
+my @array;
+
 while (my $ref = $sth->fetchrow_hashref()) {
-    print "<a href=\"#1\"> $ref->{'title'} </a>\n";
-  }
+push(@array,$ref->{'title'});
+$count++;  
+}
+my $id=0;
+print "<select size=\"$count\">";
+foreach (@array) {
+$id++;	
+print "<option    value=\"$_\" id=\"$id\" onclick=\"select(this)\">$_</option>";
+
+}
+print "</select>";  
