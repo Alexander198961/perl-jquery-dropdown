8d2b2efcd3e03a8e5fa43d7cdee29fac91621803
alexander <alexander198961@gmail.com>
2016-12-07 01:34:50 -0500

    fix

--- viewinfo.pl
+++ viewinfo.pl
@@ -3,63 +3,62 @@ use strict;
 use warnings;
 use CGI;
 use DBI;
+use DBD::Oracle;
+#exit(1);
 my $dbh;
 open( my $fh, '>>', "logfile" );
 
-my $table_name="tutorials";
-my $column_name="title";
-# Or oracle:db_name
+my $table_name="dba_tables";
+my $column_name="table_name";
 
-my $db_name="mysql:test";
-#my $db_name="oracle:db_name_here";
-
-
-my $user="root";
-
-my $password="root";
+my $db_name="Oracle:xe";
+my $user="SYSTEM";
+my $password="ps";
 
 
 print qq(Content-type: text/html\n\n);
+
 eval
 {
-	 #$ENV{ORACLE_SID} = $;
-	 #  $dbh = DBI->connect( "dbi:Oracle:", "", "", { ora_session_mode => ORA_SYSDBA } );
-	#$dbh = DBI->connect("dbi:Oracle:$dbname", $user, $passwd);
-	$dbh = DBI->connect("DBI:$db_name",
+	$dbh = DBI->connect("DBI:Oracle:xe",
                          $user, $password,
                          {'RaiseError' => 1});
 };
 if($@)
 {
+	print $@;
 	print $fh $@;
 	die("couldn't connect $@");
 }
-
 
 my $query = new CGI;
 my $name=$query->param('name');
 my $sth;
 eval
 {
-my $myquery1 = "SELECT $column_name  from $table_name where title like '%$name%'";
+
+my $myquery1 = "SELECT $column_name  from $table_name where $column_name like '%$name%'";
 $sth = $dbh->prepare($myquery1);
 $sth->execute();
 };
 if($@)
 {
-  print $fh $@;
+	 print $@;
+	print $fh $@;
   die("coldn't connect $@");  
 }
 my $count=0;
 my @array;
 
-while (my $ref = $sth->fetchrow_hashref()) {
-push(@array,$ref->{'title'});
+while (my @rows = $sth->fetchrow_array) {
+	
+push(@array,$rows[0]);
 $count++;  
 }
 my $id=0;
+use Data::Dumper;
 print "<select size=\"$count\">";
-foreach (@array) {
+foreach  (@array) {
 $id++;	
 print "<option    value=\"$_\" id=\"$id\" onclick=\"select(this)\">$_</option>";
 
